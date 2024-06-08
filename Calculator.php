7b50c4f303017b725a0efdb5aeb862bc8f3501ba
apglynn <apglynn@gmail.com>
2024-06-08 14:27:26 +0100

    first commit

---
+++ Calculator.php
@@ -0,0 +1,12 @@
+// src/Calculator.php
+<?php
+
+class Calculator {
+    public function add($a, $b) {
+        return $a + $b;
+    }
+
+    public function subtract($a, $b) {
+        return $a - $b;
+    }
+}
---
+++ tests/CalculatorTest.php
@@ -0,0 +1,16 @@
+// tests/CalculatorTest.php
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class CalculatorTest extends TestCase {
+    public function testAdd() {
+        $calculator = new Calculator();
+        $this->assertEquals(4, $calculator->add(2, 2));
+    }
+
+    public function testSubtract() {
+        $calculator = new Calculator();
+        $this->assertEquals(0, $calculator->subtract(2, 2));
+    }
+}
